1df4dc1269ca46a3146e35bccd8cedd70b568465
Kamil Marczak <kamilm@you2.pl>
2024-11-04 09:24:59 +0100

    Update page.tsx

--- app/(app)/project/[slug]/page.tsx
+++ app/(app)/project/[slug]/page.tsx
@@ -1,9 +1,13 @@
-import { useRouter } from 'next/router';
 import React from 'react';
 
-export default function ProjectPage() {
-    const router = useRouter()
+type ProjectPageProps = {
+    params: Promise<{ slug: string }>;
+};
+
+export default async function ProjectPage({ params }: ProjectPageProps) {
+    const { slug } = await params;
+    
     return (
-        <div className='pt-24 container mx-auto'>ProjectPage - WiP - {router.query.slug}</div>
+        <div className='pt-24 container mx-auto'>ProjectPage - WiP - {slug}</div>
     );
 }
