2a734673ad28965a339452fd03aef48c2514740d
Kamil Marczak <79667721+qamarq@users.noreply.github.com>
2025-03-18 12:57:29 +0100

    fix: middleware

--- middleware.ts
+++ middleware.ts
@@ -5,5 +5,5 @@ export default createMiddleware(routing)
 
 export const config = {
   // Match only internationalized pathnames
-  matcher: ['/', '/(pl|en)/:path*'],
+  matcher: ['/((?!api|trpc|_next|_vercel|admin|.*\\..*).*)'],
 }
